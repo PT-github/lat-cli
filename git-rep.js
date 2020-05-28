c9fbae557bc672f3b8073abf400e74d34cfab16a
pengtao <pengtao_it@163.com>
2020-05-28 18:23:27 +0800

    修改package.json

--- git-rep.js
+++ git-rep.js
@@ -2,7 +2,7 @@
  * @Author: PT
  * @Date: 2020-05-27 15:37:26
  * @LastEditors: PT
- * @LastEditTime: 2020-05-28 17:47:38
+ * @LastEditTime: 2020-05-28 18:21:48
  * @Description: 仓库列表
  */ 
 module.exports = [
@@ -10,16 +10,34 @@ module.exports = [
     name: 'APP',
     desc: '移动端框架，技术栈vue，UI为：vant-ui，集成了pxtorem以及多环境配置',
     git: {
-      https: 'https://github.com/PT-github/frontframe.git#master',
-      ssh: 'git@github.com:PT-github/frontframe.git#master'
+      https: 'https://github.com/PT-github/test.git#master',
+      ssh: 'git@github.com:PT-github/test.git#master'
     }
   },
   {
     name: 'PC',
     desc: 'PC端框架，技术栈vue，UI为：element-ui',
     git: {
-      https: 'https://github.com/PT-github/frontframe.git#frontiframeForPC',
-      ssh: 'git@github.com:PT-github/frontframe.git#frontiframeForPC'
+      https: 'https://github.com/PT-github/test.git#test',
+      ssh: 'git@github.com:PT-github/test.git#test'
     }
   }
 ]
+// module.exports = [
+//   {
+//     name: 'APP',
+//     desc: '移动端框架，技术栈vue，UI为：vant-ui，集成了pxtorem以及多环境配置',
+//     git: {
+//       https: 'https://github.com/PT-github/frontframe.git#master',
+//       ssh: 'git@github.com:PT-github/frontframe.git#master'
+//     }
+//   },
+//   {
+//     name: 'PC',
+//     desc: 'PC端框架，技术栈vue，UI为：element-ui',
+//     git: {
+//       https: 'https://github.com/PT-github/frontframe.git#frontiframeForPC',
+//       ssh: 'git@github.com:PT-github/frontframe.git#frontiframeForPC'
+//     }
+//   }
+// ]
