4dd3b8d4ece7bd601f8b07a9fb41e4fdf875fb18
pengtao <pengtao_it@163.com>
2020-07-02 10:20:03 +0800

    lat 脚手架 优化 增加公共模块的动态添加

--- bin/lat.js
+++ bin/lat.js
@@ -1,9 +1,67 @@
 #!/usr/bin/env node
 
 const program = require('commander')
-let package = require('../package.json')
+const minimist = require('minimist')
+const chalk = require('chalk')
 
-program.version(package.version)
+program
+  .version(require('../package.json').version)
   .usage('<command>')
-  .command('create', '创建新项目')
-  .parse(process.argv)
+
+program
+  .command('create <app-name>')
+  .description('create a new project powered by lat-cli')
+  .option('-g, --git [message]', 'Force git initialization with initial commit message')
+  .option('-f, --force', 'Overwrite target directory if it exists')
+  .action((name, cmd) => {
+    const options = cleanArgs(cmd)
+
+    
+
+    if (minimist(process.argv.slice(3))._.length > 1) {
+      console.log(chalk.yellow('\n Info: You provided more than one argument. The first one will be used as the app\'s name, the rest are ignored.'))
+    }
+    // --git makes commander to default git to true
+    if (process.argv.includes('-g') || process.argv.includes('--git')) {
+      options.forceGit = true
+    }
+    require('../lib/create')(name, options)
+  })
+
+// enhance common error messages
+const enhanceErrorMessages = require('../lib/util/enhanceErrorMessages')
+
+enhanceErrorMessages('missingArgument', argName => {
+  return `Missing required argument ${chalk.yellow(`<${argName}>`)}.`
+})
+
+enhanceErrorMessages('unknownOption', optionName => {
+  return `Unknown option ${chalk.yellow(optionName)}`
+})
+
+enhanceErrorMessages('optionMissingArgument', (option, flag) => {
+  return `Missing required argument for option ${chalk.yellow(option.flags)}` + (
+    flag ? `, got ${chalk.yellow(flag)}` : ''
+  )
+})
+
+program.parse(process.argv)
+
+function camelize (str) {
+  return str.replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '')
+}
+
+// commander passes the Command object itself as options,
+// extract only actual options into a fresh object.
+function cleanArgs (cmd) {
+  const args = {}
+  cmd.options.forEach(o => {
+    const key = camelize(o.long.replace(/^--/, ''))
+    // if an option is not present and Command has a method with the same name
+    // it should not be copied
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key]
+    }
+  })
+  return args
+}
--- git-rep.js
+++ git-rep.js
@@ -2,7 +2,7 @@
  * @Author: PT
  * @Date: 2020-05-27 15:37:26
  * @LastEditors: PT
- * @LastEditTime: 2020-05-29 17:11:45
+ * @LastEditTime: 2020-07-02 08:53:47
  * @Description: 仓库列表
  */ 
 module.exports = [
@@ -10,34 +10,38 @@ module.exports = [
     name: 'APP',
     desc: '移动端框架，技术栈vue，UI为：vant-ui，集成了pxtorem以及多环境配置',
     git: {
-      url: 'https://github.com/PT-github/frontframe.git#master',
+      url: 'https://github.com/PT-github/test.git#master',
       type: 'github'
-    }
+    },
+    // 公共模块
+    common: [
+      {
+        name: '登陆模块',
+        value: 'login'
+      },
+      {
+        name: '测试模块',
+        value: 'test'
+      },
+    ]
   },
   {
     name: 'PC',
     desc: 'PC端框架，技术栈vue，UI为：element-ui',
     git: {
-      url: 'https://github.com/PT-github/frontframe.git#frontiframeForPC',
+      url: 'https://github.com/PT-github/test.git#test',
       type: 'github'
-    }
+    },
+    // 公共模块
+    common: [
+      {
+        name: '登陆模块',
+        value: 'login'
+      },
+      {
+        name: '系统模块',
+        value: 'system'
+      }
+    ]
   }
 ]
-// module.exports = [
-//   {
-//     name: 'APP',
-//     desc: '移动端框架，技术栈vue，UI为：vant-ui，集成了pxtorem以及多环境配置',
-//     git: {
-//       https: 'https://github.com/PT-github/frontframe.git#master',
-//       ssh: 'git@github.com:PT-github/frontframe.git#master'
-//     }
-//   },
-//   {
-//     name: 'PC',
-//     desc: 'PC端框架，技术栈vue，UI为：element-ui',
-//     git: {
-//       https: 'https://github.com/PT-github/frontframe.git#frontiframeForPC',
-//       ssh: 'git@github.com:PT-github/frontframe.git#frontiframeForPC'
-//     }
-//   }
-// ]
